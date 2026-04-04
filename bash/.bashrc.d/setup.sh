e6885630d0e5aaefae421a0425d1bccbf28bd097
ghgwon <ghgwon@localhost>
2026-04-04 10:23:41 +0900

    스크립트 통합

--- bash/.bashrc.d/setup.sh
+++ bash/.bashrc.d/setup.sh
@@ -4,5 +4,31 @@ export PATH="$PATH:$HOME/.local/bin"
 # npm 패키지 매니저로 깐 모듈들 PATH 잡기
 export PATH="$PATH:$HOME/.nvm/versions/node/v22.20.0/lib/node_modules/@mermaid-js/mermaid-cli/node_modules/.bin"
 
-# LazyVim 사용을 위한 디폴트 터미널 설정 변경(xterm-256color -> wezterm)
-# export TERM=wezterm
+# zoxide 사용을 위한 eval(cd에 대한 zoxide alias는 setup.sh에서 자체적으로 적용중임)
+eval "$(zoxide init bash)"
+
+
+# kenGwon alias
+alias vim='nvim'
+alias vi='nvim'
+
+alias ls='eza --color=always'
+alias ll='eza --color=always --long --bytes --all' 
+# alias ls='ls -Cv --color=auto'
+# alias ll='ls -alFv --color=auto'
+# alias lh='ls -alFvh --color=auto'
+# alias la='ls -A --color=auto'
+# alias l='ls -CFv --color=auto'
+#
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias gr='grep -r -n -i'
+
+
+alias python='python3'
+alias cat='batcat'
+
+alias bmake='bear --append -- make'
+alias bmakef='bear -- make'          # f = fresh
+
