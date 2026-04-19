d5026225a497edcb3b7b5473ae18e1b4913defdf
kenGwon <kgh9959@gmail.com>
2026-04-19 21:27:01 +0900

    nvim 파일 변경발생시 바로 nvim에 반영. (checktime 자동화)

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -7,3 +7,26 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
+-- 외부에서 파일이 변경되면 자동으로 버퍼에 반영
+vim.opt.autoread = true
+vim.opt.updatetime = 1000
+
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  pattern = "*",
+  callback = function()
+    if vim.fn.mode() ~= "c" then
+      vim.cmd("checktime")
+    end
+  end,
+})
+
+-- 1초마다 타이머로 checktime 호출 (포커스 이동 없이도 실시간 반영)
+local timer = vim.uv.new_timer()
+timer:start(1000, 1000, function()
+  vim.schedule(function()
+    if vim.fn.mode() ~= "c" then
+      vim.cmd("checktime")
+    end
+  end)
+end)
+
